cc08047d6848a553a09b9379b4363983ade2206b
james2doyle <james2doyle@gmail.com>
2017-05-31 21:51:09 -0700

    make sure genesis block is a made of known values

--- src/GenesisBlock.php
+++ src/GenesisBlock.php
@@ -13,6 +13,6 @@ class GenesisBlock extends Block
     public function __construct()
     {
         // just some random details for the first block
-        return parent::__construct(0, '0', time(), 'my genesis block!!', '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7');
+        return parent::__construct(0, '0', 1465154705, 'my genesis block!!', '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7');
     }
 }
